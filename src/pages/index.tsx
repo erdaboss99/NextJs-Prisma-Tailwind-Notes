80ccfedf0e4edaab847e97e49d520a901b9c0b1c
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-02-06 22:11:02 +0100

    Implement "List notes feature"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps } from 'next';
 import { useState } from 'react';
+import { prisma } from 'lib/prisma';
+import { useRouter } from 'next/router';
 
 interface FormData {
 	title: string;
@@ -7,8 +9,21 @@ interface FormData {
 	id: string;
 }
 
-const Home: NextPage = () => {
+interface Notes {
+	notes: {
+		id: string;
+		title: string;
+		content: string;
+	}[];
+}
+
+const Home = ({ notes }: Notes) => {
 	const [form, setForm] = useState<FormData>({ title: '', content: '', id: '' });
+	const router = useRouter();
+
+	const refreshData = () => {
+		router.replace(router.asPath);
+	};
 
 	async function create(data: FormData) {
 		try {
@@ -18,7 +33,10 @@ const Home: NextPage = () => {
 					'Content-Type': 'application/json',
 				},
 				method: 'POST',
-			}).then(() => setForm({ title: '', content: '', id: '' }));
+			}).then(() => {
+				setForm({ title: '', content: '', id: '' });
+				refreshData();
+			});
 		} catch (error) {
 			console.log(error);
 		}
@@ -60,8 +78,38 @@ const Home: NextPage = () => {
 					Add +
 				</button>
 			</form>
+			<div className='w-auto min-w-[25%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch'>
+				<ul>
+					{notes.map((note) => (
+						<li key={note.id} className='border-b border-gray-600 p-2'>
+							<div className='flex justify-between'>
+								<div className='flex-1'>
+									<h3 className='font-bold'>{note.title}</h3>
+									<p className='text-sm'>{note.content}</p>
+								</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
 		</>
 	);
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const notes = await prisma.note.findMany({
+		select: {
+			id: true,
+			title: true,
+			content: true,
+		},
+	});
+
+	return {
+		props: {
+			notes,
+		},
+	};
+};
