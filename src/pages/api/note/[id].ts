f85c61ac8323c8b7cc26b9053ce971a0e9f0597c
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-02-06 22:43:29 +0100

    Add "Update note endpoint"

--- src/pages/api/note/[id].ts
+++ src/pages/api/note/[id].ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const noteId = req.query.id;
+	const { title, content } = req.body;
 
 	if (req.method === 'DELETE') {
 		const note = await prisma.note.delete({
@@ -10,7 +11,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		});
 
 		res.json(note);
+	} else if (req.method === 'PUT') {
+		const note = await prisma.note.update({
+			where: { id: Number(noteId) },
+			data: {
+				title,
+				content,
+			},
+		});
+
+		res.json(note);
 	} else {
-		console.log('Note could not be deleted');
+		console.log('Note could not be updated or deleted');
 	}
 }
